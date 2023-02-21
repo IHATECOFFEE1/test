7ebcf2d7a47b068d8bcf2b4bd8c914879a82d200
Felipe Souza Dias <fe_souzadias@hotmail.com>
2023-02-21 01:58:00 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TESTING CI/CD PIPELINE
         </p>
         <a
           className="App-link"
