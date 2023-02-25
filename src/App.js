d8dbbea10d3834cf0a1ad7aab7f236c9e7122ab9
Felipe Souza Dias <fe_souzadias@hotmail.com>
2023-02-25 11:57:48 -0500

    testing 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TESTING CI/CD PIPELINE
+          TESTING CI/CD PIPELINE WITH GITHUB ACTIONS
         </p>
       </header>
     </div>
